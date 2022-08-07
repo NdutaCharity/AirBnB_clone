d91da75e4b14558fa525881fd2c1d641c180529c
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-08-07 23:06:30 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,54 @@
 #!/usr/bin/python3
-""" Parent class that defines common attributes and
-methods for other classes
-"""
+"""Defines a class Base"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
-    """Defines all common attributes/methods
-    """
+    """ Class that defines properties of base """
+
     def __init__(self, *args, **kwargs):
-        """Initializes all attributes
-        """
+        """ Creates new instances of Base """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
         else:
-            f = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
-                if key != '__class__':
-                    setattr(self, key, value)
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for (key, value) in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
-        """returns class name, id and attribute dictionary
+        """Returns a string represation of class details.
+
+        Returns:
+            str: class details
         """
-        class_name = "[" + self.__class__.__name__ + "]"
-        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        return class_name + " (" + self.id + ") " + str(dct)
+        string = "["
+        string += str(self.__class__.__name__) + '] ('
+        string += str(self.id) + ') ' + str(self.__dict__)
+        return string
 
     def save(self):
-        """updates the last update time
+        """Update public instance attribute updated_at with current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of an instance."""
+        """Returns a dictionary containing all key/values of __dict__ of
+        the instance.
 
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        Returns:
+            dict: key/value pairs.
+        """
+        dict_ = self.__dict__.copy()
+        dict_['__class__'] = self.__class__.__name__
+        dict_['created_at'] = self.created_at.isoformat()
+        dict_['updated_at'] = self.updated_at.isoformat()
+        return dict_

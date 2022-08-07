bd23fc62d94e1a53862721e9b234ec929baf9f75
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-08-08 02:16:04 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -8,3 +8,7 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Defines amenities that user can choose from to offer at its place"""
     name = ""
+  def __init__(self, *args, **kwargs):
+        """Creates new instances of Amenity.
+        """
+        super().__init__(*args, **kwargs)
